33a71776b3b493be48aa2705ec93c0c36c1140fd
Michael Cassidy <mc.mathematics@gmail.com>
2025-09-11 13:23:47 +1000

    checksum seems to be working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Wfdb
+# WaveformDB
 
 [![Build Status](https://github.com/microcassidy/Wfdb.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/microcassidy/Wfdb.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+
--- src/signal.jl
+++ src/signal.jl
@@ -1,47 +1,48 @@
 function get_extension_symbol(fname)
-    extension = get_extension(fname) |> lowercase
-    if extension === nothing
-        error("extension is 'nothing'")
-    elseif extension == ".mat"
-        return :matlab
-    elseif extension == ".dat"
-        return :wfdb
-    end
-    error("extension '$(extension)' unknown")
-    return
+  extension = get_extension(fname) |> lowercase
+  if extension === nothing
+    error("extension is 'nothing'")
+  elseif extension == ".mat"
+    return :matlab
+  elseif extension == ".dat"
+    return :wfdb
+  end
+  error("extension '$(extension)' unknown")
+  return
 end
 
 function read_signal(header::Header, physical::Bool)
-    # sigs = Vector{}
-    sig_info = header_signal(header)
-    fnames = filename(sig_info)
-    uniquefname = unique(fnames)
-    #TODO: fix for multi file signals
-    @assert uniquefname |> length == 1
-    uniquefname = uniquefname[1]
-    fileextension = get_extension(uniquefname)
-    format = header_signal(header)[1].format #FIXME:abstract out
-    extension = get_extension_symbol(uniquefname)
+  # sigs = Vector{}
+  sig_info = signalspecline(header)
+  fnames = filename(header)
+  uniquefname = unique(fnames)
+  #TODO: fix for multi file signals
+  @assert uniquefname |> length == 1
+  uniquefname = uniquefname[1]
+  fileextension = get_extension(uniquefname)
+  format = signalspecline(header)[1].format #FIXME:abstract out
+  extension = get_extension_symbol(uniquefname)
 
-    uniquespf = samples_per_frame(header) |> unique
-    uniform = length(uniquespf) == 1 & uniquespf[1] == 1
+  uniquespf = samples_per_frame(header) |> unique
+  uniform = length(uniquespf) == 1 & uniquespf[1] == 1
 
 
-    if extension === :wfdb
-        samples = read_binary(pop!(fnames), header, header.parentdir, header_signal(header)[1].format)
-    elseif extension === :matlab
-        fname = joinpath(header.parentdir, uniquefname)
-        samples = matread(fname) |> values |> collect
-        if length(samples) > 1
-            error("more than one matrix in .mat file")
-        end
-        samples = samples[1]
+  if extension === :wfdb
+    samples = read_binary(pop!(fnames), header, header.parentdir, signalspecline(header)[1].format)
+  elseif extension === :matlab
+    fname = joinpath(header.parentdir, uniquefname)
+    samples = matread(fname) |> values |> collect
+    if length(samples) > 1
+      error("more than one matrix in .mat file")
     end
-    if physical
-        samples = Float16.(samples)
-        dac!(samples, header)
-    end
-    return reshape(samples, nsignals(header), :)
+    samples = samples[1]
+  end
+  _checksum = checksum(samples,header)
+  if physical
+    samples = Float16.(samples)
+    dac!(samples, header)
+  end
+  return _checksum, reshape(samples, nsignals(header), :)
 end
 
 """
@@ -50,98 +51,95 @@ end
 """
 # function read_binary(fname::String, header::Header, basedir::String, F::WfdbFormat)
 function read_binary(fname::String, header::Header, basedir::String, F::WfdbFormat)
-    error(" not implemented for type: $(typeof(F))")
+  error(" not implemented for type: $(typeof(F))")
 end
 
-
-@inline function _read!(io::IOStream, cb::CircularBuffer{2,UInt16}, ::Type{WfdbFormat{format16}})
-    push!(cb, read(io, UInt8))
-    push!(cb, read(io, UInt8))
-end
-@inline _read!(io::IOStream, cb::CircularBuffer{3,UInt8}, ::Type{WfdbFormat{format212}}) = push!(cb, read(io, UInt8))
-@inline _read!(io::IOStream, cb::CircularBuffer{3,UInt8}, ::Type{WfdbFormat{format212}}, nb::Integer) = map(_ -> push!(cb, read(io, UInt8)), nb)
-function sign_extend(value::UInt16, n::Int)::UInt16
-    sign_bit = 1 << (n - 1)
-    mask = (1 << n) - 1
-    value = value & mask
-    return (value ⊻ sign_bit) - sign_bit
-end
 function read_binary(fname::String, header::Header, basedir::String, ::WfdbFormat{format16})::Vector{Int16}
-    n_signals = nsignals(header)
-    n_samples = sum(samples_per_frame(header) * samples_per_signal(header))
-    bytes_per_sample = 2
-    n_bytes = Int64(ceil(n_samples * bytes_per_sample))
-    data = Vector{UInt8}(undef, n_bytes)
-    io = open(joinpath(basedir, fname))
-    cb = CircularBuffer{2,UInt16}()
-    output = Vector{Int16}(undef, n_samples)
-    for idx in 1:n_samples
-        _read!(io, cb, WfdbFormat{format16})
-        lower, upper = UInt16(cb[1]), UInt16(cb[2])
-        upper <<= 8
-        if upper & 0x0800 != 0
-            upper |= 0xF000
-        end
-        output[idx] = reinterpret(Int16, lower | upper)
-    end
-    close(io)
-    # return data
-    # return Matrix{Int16}(undef, nsignals, samples_per_signal(header))
-    output
+  n_signals = nsignals(header)
+  n_samples = sum(samples_per_frame(header) * samples_per_signal(header))
+  bytes_per_sample = 2
+  n_bytes = Int64(n_samples * bytes_per_sample)
+  io = open(joinpath(basedir, fname))
+  output = Vector{Int16}(undef, n_samples)
+  read!(io,output)
+  return output
 
 
+  # for idx in 1:n_samples
+  #   _read!(io, cb, WfdbFormat{format16})
+  #   lower, upper = UInt16(cb[1]), UInt16(cb[2])
+  #   upper <<= 8
+  #   if upper & 0x0800 != 0
+  #       upper |= 0xF000
+  #   end
+  #   output[idx] = reinterpret(Int16, lower | upper)
+  # end
+  # close(io)
+  # return data
+  # return Matrix{Int16}(undef, nsignals, samples_per_signal(header))
+  # output
 end
 function read_binary(fname::String, header::Header, basedir::String, ::WfdbFormat{format212})
-    io = joinpath(basedir, fname) |> open
-    cb = CircularBuffer{3,UInt8}()
-    n_samples = sum(samples_per_frame(header) * samples_per_signal(header))
-    # n_samples = sum(sam)
-    n_bytes = Integer(n_samples * 1.5)
-    output = Vector{Int16}(undef, n_samples)
+  n_samples = sum(samples_per_frame(header) * samples_per_signal(header))
+  n_bytes = Int64(n_samples * 1.5)
+  output = Vector{Int16}(undef, n_samples)
 
-    shift = 4
-    mask = 0xF0
-    # lower = UInt8(0)
-    # upper = UInt8(0)
+  m = n_samples % 2
 
+  data_buffer = zeros(UInt8,n_bytes)
+  io = open(joinpath(basedir, fname))
+  read!(io,data_buffer)
+  data_buffer = UInt16.(data_buffer)
 
-    for idx in 1:n_samples
-        isempty(cb) ? _read!(io, cb, WfdbFormat{format212}, 3) : _read!(io, cb, WfdbFormat{format212})
-        lower = cb[1]
-        upper = cb[2]
-        if idx % 2 == 1
-            upper = UInt16(upper & mask) << 4
-        else
-            lower = UInt16(lower & ~mask)
-            upper = UInt16(upper) << 8
-        end
-        if upper & 0x0800 != 0
-            upper |= 0xF000
-        end
-        output[idx] = reinterpret(Int16, lower | upper)
-        # _read!(io, cb, WfdbFormat{format212})
+  for idx in 1:n_samples
+    f = popfirst!(data_buffer)
+    l = data_buffer[1] #need for next iteration
+    m = (idx -1) % 2
+    if m == 0
+      l &= 0x0F
+      l <<= 8
+      if l & 0x0800 != 0
+          l |= 0xF000
+      end
+    else
+      f >>= 4
+      f &= 0x0F
+      f <<= 8
+      if f & 0x0800 != 0
+          f |= 0xF000
+      end
     end
-    return output
+    output[idx] = reinterpret(Int16, l | f)
+  end
+  return output
 end
 
+function checksum(samples,h::Header)
+  _nsignals = nsignals(h)
+  expanded  = Int64.(reshape(samples, _nsignals,:))
+  _checksum = sum.(eachrow(expanded))
+  # @info size(_checksum)
+  return (x -> x .% 65536).(_checksum)
+end
+
+
 function dac!(samples, h::Header)
-    baselines = baseline(h)
-    adcgains = adcgain(h)
-    _samples_per_frame = samples_per_frame(h)
-    _nsignals = nsignals(h)
-
-    blocksize = sum(_samples_per_frame)
-    nblocks = Integer(length(samples) / blocksize)
-
-
-    frame_offset = accumulate(+, _samples_per_frame)
-    circshift!(frame_offset, 1)
-    frame_offset[1] = 0
-
-    for b ∈ 0:nblocks-1, j ∈ 1:_nsignals
-        for i in 1:_samples_per_frame[j] #for jagged  indexing
-            samples[i+frame_offset[j]+b*blocksize] -= baselines[j]
-            samples[i+frame_offset[j]+b*blocksize] /= adcgains[j]
-        end
+  baselines = baseline(h)
+  initialvalues = initial_value(h)
+  # @info dump(h)
+  adcgains = adc_gain(h)
+  _samples_per_frame = samples_per_frame(h)
+  _nsignals = nsignals(h)
+  # @info "initial values $(samples[1:_nsignals])"
+  @assert all(_samples_per_frame .== 1)
+  samples_per_signal = Int64(length(samples) / _nsignals)
+  linearindex = LinearIndices(reshape(samples,_nsignals,:))
+  nrow,ncol = size(linearindex)
+  for j in 1:ncol
+    for i in 1:nrow
+        idx = linearindex[i,j]
+        samples[idx] -= baselines[i]
+        samples[idx] /= adcgains[i]
     end
+  end
 end
