0148c88b957fdc1ec9e05cddf84fb10a86c3dea7
Michael Cassidy <mc.mathematics@gmail.com>
2025-09-11 13:26:00 +1000

    * test/100.json: added

---
+++ src/WaveformDB.jl
@@ -0,0 +1,37 @@
+module WaveformDB
+export get_extension
+include("utils.jl")
+export adc_gain,
+  adc_resolution,
+  adc_zero,
+  base_counter_value,
+  base_date,
+  base_time,
+  baseline,
+  block_size,
+  byte_offset,
+  checksum,
+  counter_frequency,
+  description,
+  filename,
+  format,
+  initial_value,
+  number_of_segments,
+  number_of_signals,
+  parentdir,
+  record,
+  record_name,
+  samples_per_frame,
+  samples_per_signal,
+  sampling_frequency,
+  signal_spec,
+  skew,
+  units,
+  nsignals
+include("header.jl")
+include("signal.jl")
+
+export read_signal, read_header
+using MAT
+
+end
