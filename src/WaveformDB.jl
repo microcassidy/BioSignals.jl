4e0b9b54bef68c44248750409d33d8d3046861df
Michael Cassidy <mc.mathematics@gmail.com>
2025-09-13 23:23:41 +1000

    documentation start

--- src/WaveformDB.jl
+++ src/WaveformDB.jl
@@ -1,3 +1,24 @@
+"""
+WaveformDB.jl is a A Julia-native package for reading, writing, processing, and
+plotting physiologic signal and annotation data. The core I/O functionality is
+based on the [Waveform Database (WFDB) specifications.](https://wfdb.io/).
+
+This package is heavily inspired by the original WFDB Software Package, and
+initially aimed to replicate many of its command-line APIs. However, the
+projects are independent, and there is no promise of consistency between the
+two, beyond each package adhering to the core specifications.
+
+WaveformDB.jl can be used to read an ECG signal e.g.:
+```julia
+julia> using WaveformDB
+
+julia> rdrecord("foo.hea")
+
+```
+
+Functions in WaveformDB.jl that may come handy:
+- `WaveformDB.rdrecord`: read a record
+"""
 module WaveformDB
 export get_extension
 include("utils.jl")
