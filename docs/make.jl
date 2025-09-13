e85926cc8448e8447647635578fcbf4afc7ecd0c
Michael Cassidy <mc.mathematics@gmail.com>
2025-09-13 21:06:08 +1000

    documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,17 @@
+push!(@__DIR__, "../src/")
 using Documenter
 using WaveformDB
 
 makedocs(
-  sitename="WaveformDB",
-  format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true"),
   modules=[WaveformDB],
+  sitename="WaveformDB.jl",
+authors = "Michael Cassidy",
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/microcassidy/WaveformDB.jl",
+    push_preview=true
+)
