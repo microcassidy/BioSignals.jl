4423471ad38c982ad387b94fa0bb7397b668e7fb
Michael Cassidy <mc.mathematics@gmail.com>
2025-09-21 12:53:11 +1000

    chore:cleanup

--- test/test_buffer.jl
+++
@@ -1,9 +0,0 @@
-@testset "buffer" begin
-  buffer = WaveformDB.CircularBuffer{2,UInt8}()
-  for i in 1:5
-    WaveformDB.push!(buffer, i)
-    if buffer.length == 2
-      @test buffer[2] > buffer[1]
-    end
-  end
-end
